92252d551071742e3d6e4aafe177f75a1c4efc82
Jordan Lewin <jordan@digitalsparks.com>
2013-02-13 18:33:19 -0200

    added salytics code

--- wp-content/themes/lifeboat/javascripts/app.js
+++ wp-content/themes/lifeboat/javascripts/app.js
@@ -41,3 +41,38 @@ $(window).load(function() {
     fluid: true                         // or set a aspect ratio for content slides (ex: '4x3')
   });
 });
+
+
+
+// Salytics Code
+var saTracker;
+jQuery(document).ready( function() {
+  jQuery.getScript("http://lifeboat.na4.force.com/salytics/apex/salytics__saJS", function() {
+    saTracker = new jQuery.sa();
+    saTracker.eventTrack({type: "01260000000UJC4AAO"});
+
+    if (jQuery("#email").length > 0){
+      jQuery("#email").change( function() {
+        saTracker.linkEmail({ email: this.value });
+      });
+    }
+            
+    if (jQuery(".content-share").length > 0){
+      jQuery(".content-share").click( function() {
+        saTracker.eventTrack({type: "01260000000UJE4AAO"});
+      });
+    }
+
+    if (jQuery(".channel-subscribe").length > 0){
+      jQuery(".channel-subscribe").click( function() {
+        saTracker.eventTrack({type: "01260000000UJE9AAO"});
+      });
+    }
+
+    if (jQuery(".website-comment").length > 0){
+      jQuery(".website-comment").click( function() {
+        saTracker.eventTrack({type: "01260000000UJEEAA4"});
+      });
+    }
+  });
+});
--- wp-content/themes/lifeboat/share-icons.php
+++ wp-content/themes/lifeboat/share-icons.php
@@ -1,15 +1,15 @@
               <div class="share">
                 <span class="share-label">Share<?php print (is_front_page()) ? ' Lifeboat' : ''; ?></span>
                 <span class="share-icons">
-                  <span class="icon icon-alone icon-fb st_facebook_custom" displayText="">
+                  <span class="icon icon-alone icon-fb st_facebook_custom content-share" displayText="">
                     <span aria-hidden="true" data-icon="&#xe016;"></span>
                     <span class="screen-reader-text">Facebook</span>
                   </span>
-                  <span class="icon icon-alone icon-tw st_twitter_custom" displayText="">
+                  <span class="icon icon-alone icon-tw st_twitter_custom content-share" displayText="">
                     <span aria-hidden="true" data-icon="&#xe018;"></span>
                     <span class="screen-reader-text">Twitter</span>
                   </span>
-                  <span class="icon icon-alone icon-email st_email_custom" displayText="">
+                  <span class="icon icon-alone icon-email st_email_custom content-share" displayText="">
                     <span aria-hidden="true" data-icon="&#xe028;"></span>
                     <span class="screen-reader-text">Email</span>
                   </span>
