241c2bfb15e5bd580940e9d4e338d323a2d2c1ef
Jordan Lewin <jordan@digitalsparks.com>
2013-02-20 19:15:36 -0300

    more responsive theming; homepage slider theming and images

--- wp-content/themes/lifeboat/slider-home.php
+++ wp-content/themes/lifeboat/slider-home.php
@@ -1,42 +1,28 @@
 <div id="home-slider" class="container">
   <div class="slider">
-    <div class="slide" style="background-image: url(/wp-content/uploads/2013/02/home-slider-1.jpg);">
+    <div class="slide" style="background-image: url(/wp-content/uploads/2013/02/home-feature-get-started-guide.jpg);">
       <div class="row">
         <div class="four columns"></div>
         <div class="thirteen columns offset-by-one">
-          <a href="" class="button feature">
+          <a href="/get-started/" class="button feature">
+            <span class="button-inner">
+              <span class="button-text">Where to Start?</span>
+              <span class="icon" style="background-image: url(/wp-content/themes/lifeboat/images/icon-flag.svg);"></span>
+              <span class="button-text">Check Out the Guide</span>
+            </span>
+          </a>
+        </div><!-- thirteen columns -->
+      </div><!-- row -->
+    </div><!-- slide -->
+    <div class="slide" style="background-image: url(/wp-content/uploads/2013/02/home-feature-manifesto.jpg);">
+      <div class="row">
+        <div class="four columns"></div>
+        <div class="thirteen columns offset-by-one">
+          <a href="/goodies/manifesto/" class="button feature">
             <span class="button-inner">
               <span class="button-text">Turn on Your Friends</span>
               <span class="icon" style="background-image: url(/wp-content/themes/lifeboat/images/icon-flag.svg);"></span>
               <span class="button-text">Share the Manifesto</span>
-            </span>
-          </a>
-        </div><!-- thirteen columns -->
-      </div><!-- row -->
-    </div><!-- slide -->
-    <div class="slide" style="background-image: url(/wp-content/uploads/2013/02/home-slider-2.jpg);">
-      <div class="row">
-        <div class="four columns"></div>
-        <div class="thirteen columns offset-by-one">
-          <a href="" class="button feature">
-            <span class="button-inner">
-              <span class="button-text">What is Lifeboat?</span>
-              <span class="icon" style="background-image: url(/wp-content/themes/lifeboat/images/icon-flag.svg);"></span>
-              <span class="button-text">Watch the Video</span>
-            </span>
-          </a>
-        </div><!-- thirteen columns -->
-      </div><!-- row -->
-    </div><!-- slide -->
-    <div class="slide" style="background-image: url(/wp-content/uploads/2013/02/home-slider-3.jpg);">
-      <div class="row">
-        <div class="four columns"></div>
-        <div class="thirteen columns offset-by-one">
-          <a href="" class="button feature">
-            <span class="button-inner">
-              <span class="button-text">Lorem Ipsum Dolor</span>
-              <span class="icon" style="background-image: url(/wp-content/themes/lifeboat/images/icon-flag.svg);"></span>
-              <span class="button-text">Sit Amet Adelfus</span>
             </span>
           </a>
         </div><!-- thirteen columns -->
