ee1f2ec5fb1042b7760ca83a8c847e678edbf7c2
Jordan Lewin <jordan@digitalsparks.com>
2013-02-21 06:41:13 -0300

    added favicon.ico

--- wp-content/themes/lifeboat/header.php
+++ wp-content/themes/lifeboat/header.php
@@ -19,15 +19,6 @@
   <script>document.cookie='resolution='+Math.max(screen.width,screen.height)+("devicePixelRatio" in window ? ","+devicePixelRatio : ",1")+'; path=/';</script>
   
   <!-- icons & favicons -->
-  <!-- For iPhone 4 -->
-  <link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/h/apple-touch-icon.png">
-  <!-- For iPad 1-->
-  <link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/m/apple-touch-icon.png">
-  <!-- For iPhone 3G, iPod Touch and Android -->
-  <link rel="apple-touch-icon-precomposed" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/l/apple-touch-icon-precomposed.png">
-  <!-- For Nokia -->
-  <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/library/images/icons/l/apple-touch-icon.png">
-  <!-- For everything else -->
   <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
       
   <!-- media-queries.js (fallback) -->
