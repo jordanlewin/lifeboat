128c2f5c1d309717a105b0f4914f4634fcfc81a3
Jordan Lewin <jordan@digitalsparks.com>
2013-09-23 12:59:47 -0700

    updated homepage features - ecards and story contest

--- wp-content/themes/lifeboat/slider-home.php
+++ wp-content/themes/lifeboat/slider-home.php
@@ -1,6 +1,57 @@
 <div id="home-slider" class="container">
   <div class="slider">
 
+    <div class="slide" style="background-image: url(/wp-content/uploads/2013/09/home-feature-ecards.jpg);">
+      <a href="/goodies/friend-fact-ecards/" class="slide-link">
+        <div class="row">
+          <div class="four columns"></div>
+          <div class="thirteen columns offset-by-one">
+            <span class="button">
+              <span class="button-inner">
+                <span class="button-text">Impress Your Friends</span>
+                <span class="icon" aria-hidden="true" data-icon="&#xe008;"></span>
+                <span class="button-text">Send a Fun Fact Ecard</span>
+              </span>
+            </span><!-- button feature -->
+          </div><!-- thirteen columns -->
+        </div><!-- row -->
+      </a><!-- slide-link -->
+    </div><!-- slide -->
+
+    <div class="slide" style="background-image: url(/wp-content/uploads/2013/09/home-feature-brooklyn-treats.jpg);">
+      <a href="/goodies/story-contest/ " class="slide-link">
+        <div class="row">
+          <div class="four columns"></div>
+          <div class="thirteen columns offset-by-one">
+            <span class="button">
+              <span class="button-inner">
+                <span class="button-text">Win Brooklyn Treats</span>
+                <span class="icon" aria-hidden="true" data-icon="&#xe008;"></span>
+                <span class="button-text">Great Friend Story CONTEST!</span>
+              </span>
+            </span><!-- button feature -->
+          </div><!-- thirteen columns -->
+        </div><!-- row -->
+      </a><!-- slide-link -->
+    </div><!-- slide -->
+
+    <?php /*<div class="slide" style="background-image: url(/wp-content/uploads/2013/09/home-feature-toast.jpg);">
+      <a href="/goodies/toast/" class="slide-link">
+        <div class="row">
+          <div class="four columns"></div>
+          <div class="thirteen columns offset-by-one">
+            <span class="button">
+              <span class="button-inner">
+                <span class="button-text">Let&rsquo;s Celebrate</span>
+                <span class="icon" aria-hidden="true" data-icon="&#xe008;"></span>
+                <span class="button-text">A Toast To Great Friends</span>
+              </span>
+            </span><!-- button feature -->
+          </div><!-- thirteen columns -->
+        </div><!-- row -->
+      </a><!-- slide-link -->
+    </div><!-- slide --> */ ?>
+
     <div class="slide" style="background-image: url(/wp-content/uploads/2013/05/home-feature-friendship-report.png);">
       <a href="/friend-blog/report13-findings/" class="slide-link">
         <div class="row">
